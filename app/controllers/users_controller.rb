5e13a2b8f51e2becf7137ab054c7d4c9bdb13baa
Spencer Cornelia <cornelia.spencer@yahoo.com>
2015-06-21 14:52:36 -0700

    user sign up and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,21 @@ class UsersController < ApplicationController
 		render :index
 	end
 
+	def new
+		@user = User.new
+		render :new
+	end
+
+	def create
+		user_params = params.require(:user).permit(:name, :email, :password, :phone_number)
+		@user = User.create(user_params)
+		login(@user)
+		redirect_to "/users/#{@user.id}"
+	end
+
+	def show
+		@user = User.find(params[:id])
+		render :show
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  #would prefer having a root route
 
-  root to: "users#index"
+  root to: "welcome#index"
+
+  get "/", to: "welcome#index"
 
   get "/users", to: "users#index"
+
+  get "/users/new", to: "users#new"
+
+  post "/users", to: "users#create"
+
+  get "/users/:id", to: "users#show", as: "user_show"
+
+  #need a "sign_in" path
 
 end
