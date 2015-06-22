e87dbe83d1836b7f4413b1168e6f52faacc5c634
Spencer Cornelia <cornelia.spencer@yahoo.com>
2015-06-22 13:33:45 -0700

    navbar and user sign in fixed

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,17 +1,21 @@
 module SessionsHelper
 	def login(user)
 		session[:user_id] = user.id 
-		current_user = user 
+		@current_user = user 
 	end
 
 	def current_user
   		@current_user ||= User.find_by_id(session[:user_id])
-  	end
+  end
 
-  	def logged_in?
-  		if current_user == nil
+  def logged_in?
+  	if @current_user == nil
   			redirect_to "/sign_in"
-  		end
   	end
+	end
+
+  def logout
+    @current_user = session[:user_id] = nil
+  end
 	
 end
